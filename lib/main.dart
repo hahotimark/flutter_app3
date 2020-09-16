a80574114c223cab68e1497640e7af9a0fc92ccc
hahotimark <hahotim@gmail.com>
2020-09-16 16:39:11 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Container(
+    color: Colors.green,
+    child: Row(
+      textDirection: TextDirection.ltr,
+      children: [
+        Image.network("https://upload.wikimedia.org/wikipedia/en/thumb/6/6b/Jobbik_logo_2020_%28no_wordmark%29.png/180px-Jobbik_logo_2020_%28no_wordmark%29.png"),
+        Text(
+          "Ez egy app",
+          style: const TextStyle(),
+        textDirection: TextDirection.ltr,
+        )
+      ],
+    ),
+  ));
+}
